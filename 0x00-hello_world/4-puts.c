990053b918d7ab5308255ce77a042eae7928a67c
yadatabari <yadatabari@gmail.com>
2023-08-10 03:56:18 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/* entry point is main
- * return:zero*/
+/* entry point is main*/
+ /* return zero*/
 int main(void)
 {
-	char Mystr[] = "Programming is like building a multilingual puzzle";
-	puts(Mystr);
-        
-return (0);
+     char Mystr[] = "\"Programming is like building a multilingual puzzle\n";
+     puts(Mystr);
+     return (0);
 }
