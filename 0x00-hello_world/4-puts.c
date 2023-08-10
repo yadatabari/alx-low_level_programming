9670e04e8066f109ae20042b3dcffadbea660661
yadatabari <yadatabari@gmail.com>
2023-08-10 06:10:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
-/*main is entry point*/
- /* return Always zero*/
-
+/**
+*main - entry point
+*return: Always 0 (success)
+*/
 int main(void)
-{     
-     puts("\"Programming is like building a multilingual puzzle\n");
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
 
-     return 0;
+return (0);
 }
