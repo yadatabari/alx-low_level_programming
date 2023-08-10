70dc8b0ebb8b1ba71068ebf998ecc96660fb7a82
yadatabari <yadatabari@gmail.com>
2023-08-10 08:46:50 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ int main(void)
 	printf("Size of a char: %zu byte(s)\n", sizeof(a));
 	printf("Size of an int: %zu byte(s)\n", sizeof(b));
 	printf("Size of a long init: %zu byte(s)\n", sizeof(d));
-	printf("Size of a long long init: %lu byte(s)\n", sizeof(e));
-	printf("Size of a float: %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long init: %u byte(s)\n", sizeof(e));
+	printf("Size of a float: %u byte(s)\n", sizeof(c));
 	return (0);
 }
