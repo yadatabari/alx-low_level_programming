8a8350ca1ff5e88b360059012d9413da9375a81b
yadatabari <yadatabari@gmail.com>
2023-08-10 03:36:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
 	//string initialization
-	char Mystr[52] = "Programming is like building a multilingual puzzle";
-	
-	puts(Mystr);//writing the string to stdout
+	char Mystr[100] = "Programming is like building a multilingual puzzle"
+	puts(Mystr); //writing the string to stdout
         
-	return (0);
+return (0);
 }
