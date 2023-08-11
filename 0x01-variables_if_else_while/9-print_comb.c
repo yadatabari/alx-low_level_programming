4f01bd4137b6a1e207940fc58fbc1cb922199f28
yadatabari <yadatabari@gmail.com>
2023-08-11 15:46:30 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,13 @@ int main(void)
 	/*For loop will check the condition*/
 	for (a = 0 ; a < 10 ; a++)
 	{
-		if (a == 9)
-			putchar(a +  '0');
-		else
+		putchar(a + '0');
+		if (a != 9)
 		{
-			putchar(a + '0');
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
