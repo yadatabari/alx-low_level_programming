35869e5ad5bf97e0615fae4676b44102aaed59d1
yadatabari <yadatabari@gmail.com>
2023-08-11 12:22:00 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always (0)
+ */
+int main(void)
+{
+	int a;
+	/*For loop will check the condition*/
+	for (a = 0 ; a < 10 ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
