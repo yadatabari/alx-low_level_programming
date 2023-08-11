99e83c5f339c969015bcff7d377ed434c66a0eb4
yadatabari <yadatabari@gmail.com>
2023-08-11 10:01:59 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always (0)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+	/*loop statement that will check the condition*/
+	while (alphabet <= 'z')
+	{
+		printf("%c", alphabet);
+		alphabet++;
+	}
+	return (0);
+}
