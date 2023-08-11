e4e7439834a8c644803edd4c2749a3979aa829d6
yadatabari <yadatabari@gmail.com>
2023-08-11 12:13:25 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always (0)
+ */
+int main(void)
+{
+	int a;
+	/*for loop will check the condition*/
+	for (a = 0 ; a < 10 ; a++)
+	{
+		printf("%d", a);
+	}
+		return (0);
+}
