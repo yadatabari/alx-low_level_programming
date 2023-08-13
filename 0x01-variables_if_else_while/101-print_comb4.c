2437bb0307b70818f210657ab5b11f58dcd62fda
yadatabari <yadatabari@gmail.com>
2023-08-13 11:20:36 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * Return: always (0)
+ */
+int main(void)
+{
+	int i, j, k;
+	/*For loop will check the condition*/
+	for (i = 0 ; i <= 7 ; i++)
+	{
+	for (j = i + 1 ; j <= 8 ; j++)
+	{
+		for (k = j + 1 ; k <= 9 ; k++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(k + '0');
+		if (i < 7 || (j < 8 && k < 9))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
