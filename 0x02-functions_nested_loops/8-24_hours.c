01a5112fbb23b8bc9e9c911463433d4ab640f917
yadatabari <yadatabari@gmail.com>
2023-08-15 15:48:37 -0700

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,21 +5,27 @@
  */
 void jack_bauer(void)
 {
-	int i, j, k;
+	int i, j, k, m;
 	/*For loop will check the condition*/
-	for (i = 0 ; i <= 59 ; i++)
+	for (i = 0 ; i <= 2 ; i++)
 	{
-		for (j = 0 ; j <= 59 ; j++)
+		for (j = 0 ; j <= 9 ; j++)
 		{
-			for (k = 0 ; k <= 23 ; k++)
+			if ((i <= 1 && j <= 9) || (i <= 2 && j <= 3))
 			{
-			_putchar(k / 10 + '0');
-			_putchar(k % 10 + '0');
-			_putchar(':');
-			_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
+			for (k = 0 ; k <= 5 ; k++)
+				{
+			for (m = 0 ; m <= 9 ; m++)
+				{
+			_putchar(i + '0');
+			_putchar(j + '0');
+			_putchar(58);
+			_putchar(k + '0');
+			_putchar(m + '0');
 			_putchar('\n');
 		}
 	}
+}
+}
 }
 }
