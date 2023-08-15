4242bec6629511ed37a9ea10fcaf368e269c13cf
yadatabari <yadatabari@gmail.com>
2023-08-15 12:14:15 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - printing alphabet ten times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+	/*For loop will check the condition*/
+	i = 0;
+	while (i < 10)
+	{
+		for (j = 'a' ; j < 'z' ; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
