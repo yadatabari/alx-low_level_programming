33af74189c852206e807bfd7db2ba9a0318f9fea
yadatabari <yadatabari@gmail.com>
2023-08-15 09:50:53 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - make the alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char i;
+	/*For loop will check the condition*/
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
