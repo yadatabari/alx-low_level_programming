8840039979e80a2ac95591b63efe9c653bb294c0
yadatabari <yadatabari@gmail.com>
2023-08-16 01:48:51 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add - Addition of two integers
- * @n1 - the first combuted parameter 
- * @n2 - the first combuted  Parameter
+ * @n1: the first combuted parameter
+ * @n2: the first combuted  Parameter
  * Return: result
  */
 int add(int n1, int n2)
