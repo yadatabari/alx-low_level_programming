15986ab56c09f47664847fe8cc080798ac619390
yadatabari <yadatabari@gmail.com>
2023-08-18 09:26:55 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - check the uppercase alphabet
+ *@c: the checked alphabet
+ *Return: 0 or 1
+ *
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+}
