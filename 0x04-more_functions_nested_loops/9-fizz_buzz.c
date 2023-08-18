50acf71fc83e4040c3afcb8f8b0827d91557d272
yadatabari <yadatabari@gmail.com>
2023-08-18 16:23:22 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	/*for loop will check the condition*/
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+		printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			if (i == 100)
+			{
+				printf("Buzz ");
+				printf("\n");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
