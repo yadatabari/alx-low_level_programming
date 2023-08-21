4503d6061d2b304adf1bba846c9ad3415ee0fd16
yadatabari <yadatabari@gmail.com>
2023-08-21 16:31:13 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *puts2 - function that print ever character
+ *@s: parameter
+ *Return: void
+ *
+ */
+void puts2(char *str)
+{
+	int me = 0;
+	int t = 0;
+	char *n = str;
+	int x;
+	while (*n != '\0')
+	{
+		n++;
+		me++;
+	}
+	t = me -1;
+	for (x = 0; x <= t; x++)
+	{
+		if(x % 2 == 0)
+		{
+			_putchar(str[x]);
+		}
+	}
+	_putchar('\n');
+}
+
