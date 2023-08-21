c7d25c5ddcead1b1366b71f864ecdc222bdf6748
yadatabari <yadatabari@gmail.com>
2023-08-21 11:31:03 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *
+ *swap_int - function that swap two parameter
+ *@a: First parameter
+ *@b: Second parameter
+ *Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
