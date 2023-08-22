5af21493a70a3ccc6c3c45a402d00ccde5599ccb
yadatabari <yadatabari@gmail.com>
2023-08-22 13:15:55 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,21 +2,21 @@
 /**
  *_atoi - functiontha convert string to an integer
  *@s:parameter
- *Return: the int conerted 
+ *Return: the int conerted
  *
  */
 int _atoi(char *s)
 {
 	int i, j, n, len, f, digit;
+	/*initialization*/
 	i = 0;
 	j = 0;
 	n = 0;
 	len = 0;
 	f = 0;
 	digit = 0;
-	while(s[len] != '\0')
-	len++;
-	
+	while (s[len] != '\0')
+		len++;
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
@@ -29,11 +29,11 @@ int _atoi(char *s)
 			digit = -digit;
 		n = n * 10 + digit;
 		f = 1;
-		if (s[i+1] <'0'|| s[i+1] > '9')
+		if (s[i + 1] < '0' || s[i + 1] > '9')
 			break;
-		f=0;
-	}
-	i++;
+		f = 0;
+		}
+		i++;
 	}
 if (f == 0)
 return (0);
