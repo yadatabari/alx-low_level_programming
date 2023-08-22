d1bf3387df89b9840557d1bd89960ec9c29469db
yadatabari <yadatabari@gmail.com>
2023-08-21 17:00:13 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *function that print half of the string
+ *@str: parameter
+ *Return: void
+ *
+ */
+void puts_half(char *str)
+{
+	int a;
+	int b;
+	int me = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		me++;
+	}
+	b = (me / 2);
+	if ((me % 2) == 1)
+		b = ((me + 1) / 2);
+	for (a = b; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
+
